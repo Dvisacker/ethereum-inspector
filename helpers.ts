d145bcc82bb526a03fc742cbdf32d08934814db4
dvisacker <davidvanisacker@gmail.com>
2025-05-20 03:41:41 +0900

    feat - terminal link functions

--- helpers.ts
+++ helpers.ts
@@ -12,3 +12,28 @@ export const abbreviateAddress = (address: string): string => {
 export function addressToTopic(address: string): string {
   return "0x000000000000000000000000" + address.slice(2, address.length);
 }
+
+export function createTerminalLink(text: string, url: string): string {
+  // OSC 8 is the escape sequence for hyperlinks
+  // \x1b]8;; is the start of the link
+  // \x1b\\ is the end of the link
+  return `\x1b]8;;${url}\x1b\\${text}\x1b]8;;\x1b\\`;
+}
+
+export function createEtherscanLink(address: string): string {
+  return createTerminalLink(address, `https://etherscan.io/address/${address}`);
+}
+
+export function createTenderlyLink(address: string): string {
+  return createTerminalLink(
+    address,
+    `https://dashboard.tenderly.co/contract/mainnet/${address}`
+  );
+}
+
+export function createArkhamLink(address: string): string {
+  return createTerminalLink(
+    address,
+    `https://app.arkhamintelligence.com/explorer/address/${address}`
+  );
+}
